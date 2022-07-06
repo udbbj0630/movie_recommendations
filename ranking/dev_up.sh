6820181ecd5bbe9a772615f9ea9b7d93da3bc624
udbbj0630 <udbbj0630@gmail.com>
2022-07-05 23:13:07 -0400

    rename container

--- ranking/dev_up.sh
+++ ranking/dev_up.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-docker build -f Dockerfile -t movie_recommendation/ranking:0.0 .
-docker run -it --rm -v /Users/shuangyueli/movie_recommendations/movie_recommendations/ranking:/ranking movie_recommendation/ranking:0.0 /bin/bash
+while getopts "b" arg
+do
+  case $arg in
+    b)
+      docker rmi movie_recommendation/ranking:0.0
+      docker build -f Dockerfile -t movie_recommendation/ranking:0.0 .
+      ;;
+    ?)
+      echo "Unknown args $arg... exit..."
+      exit 1
+      ;;
+  esac
+done
+docker stop ranking_dev
+docker run -it -d --rm --name ranking_dev -v /Users/shuangyueli/movie_recommendations/movie_recommendations/ranking:/ranking movie_recommendation/ranking:0.0 /bin/bash
