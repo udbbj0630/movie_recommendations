c7dc0bef2ad6dbb444bfa9235dc2f9f324042df0
udbbj0630 <udbbj0630@gmail.com>
2022-07-22 01:51:03 -0400

    golang_movie_search

--- ranking/serve_ranking_test.go
+++ ranking/serve_ranking_test.go
@@ -1,12 +1,33 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"net/http"
+)
+var buff []byte
 
-func TestAdd(t *testing.T) {
-	got := Add(1,4)
-	want := 3
+type TestResposeWriter struct{
+} 
+func (t TestResposeWriter) Header() http.Header {
+	return nil
+}
+func (t TestResposeWriter) Write(b []byte) (int, error) {
+	buff = b
+	return 0,nil
+}
+func (t TestResposeWriter) WriteHeader(statusCode int) {
+}
 
-	if got != want 	{
-		t.Fatalf(" expected %d, but got %d", want, got)
+
+func TestHandler(t *testing.T) {
+	writer := TestResposeWriter{}
+	req, _ := http.NewRequest("GET", "http://example.com", nil)
+
+	handler(writer, req)
+
+	want := "Movie List Nope,Thor: Love and Thunder,Minions: The Rise of Gru,Where the Crawdads Sing"
+	got := string(buff[:])
+	if got != want 	{ 
+		t.Fatalf(" expected %q, but got %q", want, got)
 	}
 }
