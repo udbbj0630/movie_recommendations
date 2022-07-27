d3e8afb7bd5c42a851ad1bd7b40339718a226cc3
udbbj0630 <82064903+udbbj0630@users.noreply.github.com>
2022-07-26 23:14:35 -0400

    set up load balancer

---
+++ load_balancer/init.sh
@@ -0,0 +1,16 @@
+#! /user/bin/env bash
+# Configures the nginx server upon startup.
+
+# Assign configuration values to enviroment values.
+export HOST_IP=$0
+export RANK_PORT=$1
+export REVERSE_INDEX_PORT=$2
+
+# Produce nginx configuration from template
+/usr/bin/envsubst '$HOST_IP $RANK_PORT $REVERSE_INDEX_PORT' < nginx.conf.template > /etc/nginx/nginx.conf
+
+# Show generated config for the sake of debugging.
+cat /etc/nginx/nginx.conf
+
+# Start nginx.
+nginx -g 'daemon off;' 2> start_error.log
