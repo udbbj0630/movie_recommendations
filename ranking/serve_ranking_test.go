d34be3c43948b88f0d2e26b451a7f94d3e5b7946
udbbj0630 <82064903+udbbj0630@users.noreply.github.com>
2022-07-26 22:30:15 -0400

    revese indexing setup

--- ranking/serve_ranking_test.go
+++ ranking/serve_ranking_test.go
@@ -1,33 +1,52 @@
 package main
 
 import (
-	"testing"
+	"encoding/json"
 	"net/http"
+	"testing"
 )
+
 var buff []byte
 
-type TestResposeWriter struct{
-} 
-func (t TestResposeWriter) Header() http.Header {
-	return nil
-}
-func (t TestResposeWriter) Write(b []byte) (int, error) {
-	buff = b
-	return 0,nil
-}
-func (t TestResposeWriter) WriteHeader(statusCode int) {
+type TestResponseWriter struct {
+	statusCode int;
+	header map[string][]string; 
 }
 
+func (t TestResponseWriter) Header() http.Header {
+	header := http.Header{}
+	return header
+}
+
+func (t TestResponseWriter) Write(b []byte) (int, error) {
+	buff = b
+	return len(b), nil
+}
+
+func (t TestResponseWriter) WriteHeader(statusCode int) {
+	t.statusCode = statusCode
+}
 
 func TestHandler(t *testing.T) {
-	writer := TestResposeWriter{}
-	req, _ := http.NewRequest("GET", "http://example.com", nil)
-
+	writer := TestResponseWriter{}
+	req, _ := http.NewRequest("GET", "http://localhost:8080", nil)
 	handler(writer, req)
 
-	want := "Movie List Nope,Thor: Love and Thunder,Minions: The Rise of Gru,Where the Crawdads Sing"
-	got := string(buff[:])
-	if got != want 	{ 
-		t.Fatalf(" expected %q, but got %q", want, got)
+	gotJson := buff[:]
+	got := make(map[string][]string)
+	if err := json.Unmarshal(gotJson, &got); err != nil {
+		t.Fatalf("unmarshal resp error: %s", err)
+	}
+	movieList := got["movieList"]
+	wantMovieList := []string{
+		"Nope", 
+		"Thor: Love and Thunder",
+		"Minions: The Rise of Gru",
+		"where the Crawdads Sing",
+	}
+	for i, movie := range wantMovieList {
+		if movie != movieList[i] {
+			t.Fatalf("Expected %q, but got %q", movie, movieList[i])
+		}
 	}
 }
